d39ba085af85b1fb3caaf78df958f73a5e66ae41
Yannri Zenner <zenner88@gmail.com>
2022-08-23 21:58:28 +0700

    sss

---
+++ routes/banner_link.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const banner_link = require('../services/banner_link');
+var bodyParser = require('body-parser')
+const app = express();
+
+/* GET banner_link. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await banner_link.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting banner_link `, err.message);
+    next(err);
+  }
+});
+
+/* POST banner_link. */
+router.post('/', async function(req, res, next) {
+    try {
+      res.json(await banner_link.create(req.body));
+    } catch (err) {
+      console.error(`Error while creating banner_link`, err.message);
+      next(err);
+    }
+});
+
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// for parsing multipart/form-data
+let multer = require('multer');
+let upload = multer();
+
+/* PUT banner_link */
+router.put('/:id', upload.single('file'), async function(req, res, next) {
+  console.log(req.body);
+  console.log(req.file);
+  try {
+    res.json(await banner_link.update(req.params.id, req));
+    } catch (err) {
+      console.error(`Error while updating banner_link`, err.message);
+      next(err);
+    }
+});
+
+/* DELETE banner_link */
+router.delete('/:id', async function(req, res, next) {
+    try {
+      res.json(await banner_link.remove(req.params.id));
+    } catch (err) {
+      console.error(`Error while deleting banner_link`, err.message);
+      next(err);
+    }
+});
+
+module.exports = router;
---
+++ services/banner_link.js
@@ -0,0 +1,120 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const router = require('../routes/banner_link');
+
+// upload FILE
+// parse application/json
+
+
+async function getMultiple(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT identifier, type, url, hyperlink
+    FROM banner_link LIMIT ${offset},${config.listPerPage}`
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+async function create(banner_link){
+  if (banner_link.type == "image"){
+    var result = await db.query(
+      `INSERT INTO banner_link 
+      (identifier, type, hyperlink) 
+      VALUES 
+      ("${banner_link.identifier}", "${banner_link.type}", "${banner_link.hyperlink}")` 
+    );
+  }else if(banner_link.type == "video"){
+    var result = await db.query(
+      `INSERT INTO banner_link 
+      (identifier, type, url) 
+      VALUES 
+      ("${banner_link.identifier}", "${banner_link.type}", "${banner_link.url}")` 
+    );
+  }
+  let message = 'Error in creating banner_link';
+  
+  if (result.affectedRows) {
+    message = 'banner_link created successfully';
+  }
+  
+  return {message};
+}
+
+async function update(identifier, banner_link){
+  if (banner_link.body.type == "image"){
+    var result = await db.query(
+      `UPDATE banner_link 
+      SET type="${banner_link.body.type}", hyperlink="${banner_link.body.hyperlink}", url=""
+      WHERE identifier="${identifier}"` 
+    );
+  }else if(banner_link.type == "video"){
+    var result = await db.query(
+      `UPDATE banner_link 
+      SET type="${banner_link.type}", url="${banner_link.url}", hyperlink=""
+      WHERE identifier="${identifier}"` 
+    );
+  }
+
+  let message = 'Error in updating banner_link';
+
+  if (result.affectedRows) {
+    message = 'banner_link updated successfully';
+  }
+  if (result.affectedRows && banner_link.body.type == "image") {
+    console.log("upload in")
+    // upload FILE 1
+    // handle storage using multer
+    var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads');
+      },
+      filename: function (req, file, cb) {
+        cb(null, `http://202.67.10.240:3000/images/"${identifier}".png`);
+      }
+    });
+    var upload = multer({ storage: storage });
+
+    // handle single file upload
+    router.post('/', upload.single('file'), (banner_link, res, next) => {
+      const file = banner_link.file;
+      if (!file) {
+        return res.status(400).send({ message: 'Please upload a file.' });
+      }
+      return res.send({ message: 'File uploaded successfully.', file });
+    });
+  }
+
+  return {message};
+}
+
+async function remove(identifier){
+  const result = await db.query(
+    `DELETE FROM banner_link WHERE identifier="${identifier}"`
+  );
+
+  let message = 'Error in deleting banner_link';
+
+  if (result.affectedRows) {
+    message = 'banner_link deleted successfully';
+  }
+
+  return {message};
+}
+
+module.exports = {
+  getMultiple,
+  create,
+  update,
+  remove
+}
